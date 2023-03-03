ecdafc876ce8ccf54e54e1ba0fffdb957986d63a
KuRioSTmp <112512874+KuRioSTmp@users.noreply.github.com>
2023-03-03 14:21:33 +0100

    Added Methods to DataService

--- BandsMvc/Models/DataService.cs
+++ BandsMvc/Models/DataService.cs
@@ -2,5 +2,19 @@
 {
     public class DataService
     {
+        private List<Band> Bands = new List<Band>
+        {
+
+        };
+
+        public Band GetBandById(int id)
+        {
+            return Bands.Where(b => b.Id.Equals(id)).First();
+        }
+
+        public Band[] GetAllBands()
+        {
+            return Bands.OrderBy(b => b.Name).ThenBy(b => b.Id).ToArray();
+        }
     }
 }
