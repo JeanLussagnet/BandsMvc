b30dbf775d2fad7919e321cc037d9be90c128600
KuRioSTmp <112512874+KuRioSTmp@users.noreply.github.com>
2023-03-03 14:32:47 +0100

    Added instance to Dataservice

--- BandsMvc/Controllers/BandsController.cs
+++ BandsMvc/Controllers/BandsController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BandsMvc.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BandsMvc.Controllers
 {
     public class BandsController : Controller
     {
+        DataService dataService = new DataService();
 
         [HttpGet("")]
         public IActionResult Index()
