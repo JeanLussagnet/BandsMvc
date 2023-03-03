d56ebd2a19e4440cf915ed4b79a4040f70b851c4
KuRioSTmp <112512874+KuRioSTmp@users.noreply.github.com>
2023-03-03 14:09:07 +0100

    Add project files.

---
+++ BandsMvc/Controllers/BandsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BandsMvc.Controllers
+{
+    public class BandsController : Controller
+    {
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
